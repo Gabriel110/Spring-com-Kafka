6395528a16092bf6197e7f2357d11b4bebb67090
Gabriel110 <gabrielcorreiacev@gmail.com>
2022-10-23 01:03:20 -0300

    adicionando teste de integracao

--- shop/src/test/kotlin/br/com/gabriel/shop/integracao/aplication/controller/shop/ShopControllerTest.kt
+++ shop/src/test/kotlin/br/com/gabriel/shop/integracao/aplication/controller/shop/ShopControllerTest.kt
@@ -1,11 +1,12 @@
 package br.com.gabriel.shop.integracao.aplication.controller.shop
 
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.MockMvcBuilder
+import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
@@ -18,6 +19,10 @@ class ShopControllerTest {
 
     private lateinit var mockMvc: MockMvc
 
+    companion object {
+        private const val RECUSO_SAVE = "/v1/shop/save"
+    }
+
 
     @BeforeEach
     fun setup(){
@@ -25,5 +30,13 @@ class ShopControllerTest {
             .apply<DefaultMockMvcBuilder?>(
                 SecurityMockMvcConfigurers.springSecurity()
             ).build()
+    }
+
+    @Test
+    fun `quando chamado deve retorna 401`(){
+
+        mockMvc.get(RECUSO_SAVE).andExpect {
+            status { is4xxClientError() }
+        }
     }
 }
